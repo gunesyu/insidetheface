a57684ad2705ac8b52ee7572d555b96e6b0cbb52
gunesyu <gunesyunusemre@gmail.com>
2016-01-17 15:18:52 +0200

    js hack for canonical

--- js/startApp.js
+++ js/startApp.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	$("#enter").click(function(){
-		window.location.assign("app.html");
+		window.location.assign("https://gunesyu.github.io/insidetheface/app.html");
 	});
 
 
